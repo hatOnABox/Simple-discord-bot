27c4cefc578778f179fed09b32d4390d5f3debad
John Paul Antonovich <AntonovichJP@gmail.com>
2019-04-06 15:35:56 -0700

    Init

---
+++ sample_code.py
@@ -0,0 +1,114 @@
+import discord
+import os
+import random
+import time
+import sqlite3
+import threading
+
+
+client = discord.Client()
+down = False
+runningSleep = False
+kicklistener = {}
+
+
+def backgroundSleep():
+    global down
+    global runningSleep
+
+    time.sleep(10)
+    down = False
+    runningSleep = False
+
+@client.event # event desorator/wrapper
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    global down
+    global runningSleep
+
+    if down == True and message.author.server_permissions.administrator == False:
+        if runningSleep == False:
+            t = threading.Thread(target=backgroundSleep)
+            t.start()
+            runningSleep = True
+        pass
+    else:
+
+        if message.content.lower() == "!source":
+            await client.send_file(message.channel, f"{os.getcwd()}/sample_code.txt")
+            if message.author.server_permissions.administrator == False:
+                down = True
+
+        if message.content.lower() == "!quote":
+            conn = sqlite3.connect('quotes.db')
+            c = conn.cursor()
+
+            c.execute('''CREATE TABLE IF NOT EXISTS quote (text, author)''')
+            conn.commit()
+            quotes = list(c.execute('''SELECT * FROM quote'''))
+            thingy = random.choice(quotes)
+
+            await client.send_message(message.channel, f"{thingy[0] + ' ' + thingy[1]}")
+
+
+            conn.close()
+            if message.author.server_permissions.administrator == False:
+                down = True
+
+
+        if message.content.lower() == "!commands":
+            await client.send_message(message.channel, "!quote - Prints a funny quote\n!commands - pulls this list up again\n!source - prints a txt file with this bot's source code\n!add quote - adds quote (NOTE: only works if admins use it)\n!off - turns bot off (NOTE: only works if admins use it)\n!clear - clears full char (only admins can use)")
+
+            if message.author.server_permissions.administrator == False:
+                down = True
+
+        if message.content.lower()[0:10] == "!add quote" and message.author.server_permissions.administrator == True:
+
+            if len(message.content.split("\"")) != 5:
+                await client.send_message(message.channel, "Error: Incorrect quote syntax")
+            else:
+                quote = message.content.split("\"")[1]
+                author = message.content.split("\"")[3]
+
+            conn = sqlite3.connect('quotes.db')
+            c = conn.cursor()
+            c.execute('''CREATE TABLE IF NOT EXISTS quote (text, author)''')
+            conn.commit()
+            c.execute(f'INSERT INTO quote (text, author) VALUES (?, ?)', (quote, author))
+            conn.commit()
+            conn.close()
+
+            await client.send_message(message.channel, "Add quote: " + quote + ' ' + author)
+
+
+
+        if message.content.isupper() == True and message.author.server_permissions.administrator == False:
+            await client.send_message(message.channel, str(message.author) + " Thou shall not spam caps!!!")
+
+            try:
+                if kicklistener[str(message.author)] == 3:
+                    await client.kick(message.author)
+                    kicklistener[str(message.author)] = 0
+                else:
+                    kicklistener[str(message.author)] += 1
+            except:
+                kicklistener[str(message.author)] = 0
+
+
+        if message.content.lower() == "!off" and message.author.server_permissions.administrator == True:
+            await client.send_message(message.channel, "Bot is shuting down...")
+            await client.close()
+
+
+        if message.content.lower() == "!clear" and message.author.server_permissions.administrator == True:
+            msgs = []
+            async for x in client.logs_from(message.channel, limit=1000):
+                msgs.append(x)
+            await client.delete_messages(msgs)
+
+
+client.run("ENTER YOUR TOKEN HERE")
